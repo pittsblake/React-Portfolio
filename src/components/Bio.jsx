1282e1f2756c6865bfa20aa9d61c37ddbb18d6c8
pittsblake <pittsblake1@gmail.com>
2017-11-21 18:16:24 -0500

    created onClick function to show info

---
+++ src/components/Bio.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const CardContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+const Card = styled.div`
+box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0   6px 20px 0 rgba(0, 0, 0, 0.19);
+padding-top: 5px;
+padding-bottom: 10px;
+background-color: white;
+opacity: .8;
+width: 50%;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+const Bio = () => {
+    return (
+        <CardContainer>
+            <Card>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </Card>
+        </CardContainer>
+    );
+};
+
+export default Bio;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components'
+import Bio from './Bio'
+import BikeInfo from './BikeInfo'
 
 const Container = styled.div`
     display: flex;
@@ -10,55 +12,74 @@ const Container = styled.div`
 
     div {
         display: flex;
-    position: relative;
-    background-color: tomato;
-    width: 50%;
-    padding: 50px;
+        position: relative;
+        background-color: light cyan;
+        width: 50%;
+        padding: 50px;
     } 
     
     div:first-child {
-        margin-right: 108px
+        margin-right: 0px
     }
 `
 
 const Coffee = styled.div`
-    &:after {
-        content:'';
-        position: absolute;
-        top: 0;
-        width: 0;
-        height: 0;
-        left: 100%;
-        border-top: 100vh solid tomato;
-        border-right: 100px solid transparent;
+    background-image: url("https://i.imgur.com/CKF2FXP.jpg");
+    background-size: cover;
+    background-position:center;
+    display: flex;
+    justify-content: center;
+
+    h1 {
+        height: 100px;
     }
 `
 
 const Bike = styled.div`
-    &:before {
-        content:'';
-        position: absolute;
-        top: 0;
-        width: 0;
-        height: 0;
-        right: 100%;
-        border-bottom: 775px solid tomato;
-        border-left: 100px solid transparent;
-    }
+    background-image: url("https://i.imgur.com/YZHydpd.jpg");
+    background-size: 150% 100%;
+    background-position: right center;
+    background-repeat: no-repeat;
+    display: flex;
+    justify-content: center
 `
 
-const Main = () => {
-    return (
-        <Container>
-            <Coffee>
-                <h1>Hi</h1>
-            </Coffee>
+class Main extends Component {
 
-            <Bike>
-                <h1>hi</h1>
-            </Bike>
-        </Container>
-    );
-};
+    state = {
+        showBio: false,
+        showBikeInfo: false
+    }
+
+    showBio = () => {
+        this.setState({ showBio: !this.state.showBio })
+    }
+
+    showBikeInfo = () => {
+        this.setState({ showBikeInfo: !this.state.showBikeInfo })
+    }
+
+    render() {
+        return (
+            <Container>
+                <Coffee>
+                    <h1 onClick={this.showBio}>Developer</h1>
+
+                </Coffee>
+
+                {
+                    this.state.showBio ? <Bio /> : null
+                }
+                {
+                    this.state.showBikeInfo ? <BikeInfo /> : null
+                }
+                <Bike>
+                    <h1 onClick={this.showBikeInfo}>Cyclist</h1>
+
+                </Bike>
+            </Container>
+        );
+    };
+}
 
 export default Main;
